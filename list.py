dbe8131b7c6f10243d96996f56983ee242a9e4f4
sultan <sultan_33_886@outlook.sa>
2023-01-15 10:17:11 +0300

    lablist

---
+++ list.py
@@ -0,0 +1,27 @@
+numbers_list = [2, 3, 4, 5, 15, 1, 43, 20]
+def add_list(numbers_list:int):
+    return numbers_list
+total=0
+for numbers_list in numbers_list:
+    total=total+numbers_list
+print("the sum of list",total)
+
+     
+
+def max_number(numbers_list):
+    return numbers_list
+print(numbers_list)
+
+umbers_list = []
+def odd_number(numbers):
+    pass
+x=0     
+for numbers in range(1200,2000,125):
+        if  numbers%2==0:
+            print(numbers)
+        else :
+            print("none")
+
+    
+
+
