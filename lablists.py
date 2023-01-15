693afa9abaafe8bbc6f6f34e178fb53f38610c1b
Faimubarak <102253601+Faimubarak@users.noreply.github.com>
2023-01-15 11:08:14 +0300

    l

--- lablists.py
+++ lablists.py
@@ -1,9 +1,9 @@
 #Q1
-def list_sum(lst):
-    return list_sum + (lst[1:]) 
-lst = [2, 3, 4, 5, 15, 1, 43, 20]
-print(sum(lst))
 
+lst = [2, 3, 4, 5, 15, 1, 43, 20]
+def list_sum(lst):
+    return list_sum + (lst) 
+print(sum(lst))
 
 
 #َQ2
@@ -20,12 +20,13 @@ def largest_number(numbers):
 print(largest_number([2, 3, 4, 5, 15, 1, 43, 20]))
 
 #Q3
-odd_num = [x for x in range(1200, 2000) if x %2 != 0 ]
+odd_num = [x for x in range(1200, 2000, 125) if x % x == 0 ]
+print(odd_num)
 
 #Q4
-list1 = [2, 3, 4, 5, 15, 1, 43, 20]
-new_list = list1[:5]
-print(new_list)
+
+new_list = odd_num[5:]
+print(new_list) 
 
 
 
