9f41784ab21a342ee3249fc295ecdffd8ec5e03b
MOH7117 <mohammad201027@hotmail.com>
2023-01-15 10:55:21 +0300

    lab lists

--- Lists.py
+++ Lists.py
@@ -1,15 +1,15 @@
-#Q1
-def sum_list(number):
 
-  total = 0
+# #Q1
+def sum_list(number) -> int :
+  i = 0
   for x in number:
-    total = total + x
-  return total
-my_list = [2, 3, 4, 5, 15, 1, 43, 20]
-print ("sum my_list", sum_list(my_list))
+    i = i + x
+  return i
+number = [2, 3, 4, 5, 15, 1, 43, 20]
+print ("sum my_list", sum_list(number))
 
 
-#Q2
+# #Q2
 def large_Number(list):
 
 	max = list1[0]
@@ -20,6 +20,7 @@ def large_Number(list):
 list1 = [2, 3, 4, 5, 15, 1, 43, 20]
 print("Large number is:", large_Number(list))
 
+
 #Q3
 number_list = [ x for x in range(1200,2000,125) if x % 2 != 0]
 print(number_list)
@@ -27,5 +28,5 @@ print(number_list)
 
 #Q4
 slic_List = [2, 3, 4, 5, 15, 1, 43, 20]
-new_list = slic_List[0:6]
-print(slic_List[0:6])
+new_list = slic_List[0:5]
+print(slic_List[0:5])
