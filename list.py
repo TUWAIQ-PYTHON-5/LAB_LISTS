5bc53d1563a23d0dcd7216feaf89047bf372824b
NjoudAlhazmi <njoudz19@gmail.com>
2023-01-15 16:11:10 +0300

    +new solve

--- list.py
+++ list.py
@@ -1,26 +1,34 @@
-#Q1
-mylist = [2, 3, 4, 5, 15, 1, 43, 20]
+# Q1
+my_list=[2,3,4,5,15,1,43,20]
 
-# using sum() function
-total = sum(mylist)
-print("Sum of all elements:", total)
+
+def my_function_list(my_list: list):
+    return sum(my_list)
+print(my_function_list(my_list))
 
 
 
 #Q2
-# sorting the list
-mylist.sort()
-print("Largest element is:", mylist[-1])
+def theLargest(my_list):
+	max = my_list[0]
+	for x in my_list:
+		if x > max:
+			max = x
+	return max
+
+print(2,"Largest element is:", theLargest(my_list))
+theLargest(my_list)
 
 
 
-#Q3
+
 def odd_numbers(n):
     return [x for x in range(1200,2000,125) if x%2 != 0]
 print(3,odd_numbers(0))
 
 
+
 #Q4
-sliced_list = mylist[0:5]
+sliced_list = my_list[0:5]
 print(4,sliced_list)
 
